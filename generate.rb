d0cc055d04cab0744fdbdeb65ecc64d4dd3e1804
Yacine Petitprez <yacine@kosmogo.fr>
2015-10-21 11:24:44 +0800

    Ignore already existing words

--- generate.rb
+++ generate.rb
@@ -1,8 +1,14 @@
 require 'json'
 
-DICTIONARY = JSON.parse(File.read('prob_table.json'))
+MARKOV_LINKS = JSON.parse(File.read('prob_table.json'))
+
+DICTIONARY = Hash[File.read("dico.txt").split("\n").map{|x| [x, true]}] #Deja trié
 
 NUM_LETTERS_STORED = 4
+
+def word_exists? w
+  DICTIONARY[w.strip]
+end
 
 def rand_pick_char sub_prob_array
   total_occurences = sub_prob_array.values.inject(0){ |s,x| s+=x }
@@ -22,7 +28,7 @@ def generate_word
 
   word = " "
   loop do
-    next_prob = DICTIONARY[word[-[NUM_LETTERS_STORED, word.length].min..-1]]
+    next_prob = MARKOV_LINKS[word[-[NUM_LETTERS_STORED, word.length].min..-1]]
 
     current_char = rand_pick_char(next_prob)
     word += current_char
@@ -32,4 +38,18 @@ def generate_word
   word.strip
 end
 
-puts 100.times.map{ generate_word }.join(" ")
+puts 100.times.map{
+  w = nil
+
+  loop do
+    w = generate_word
+
+    if word_exists?(w)
+      puts "Ignore #{w}*"
+    end
+
+    break unless word_exists?(w)
+  end
+
+  w
+}.join(" ")
